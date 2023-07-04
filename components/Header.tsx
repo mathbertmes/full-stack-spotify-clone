956c9ea7ab54c088e3c4d3159ce5912f3b52d15c
Matheus Bertemes <mathbertemes@gmail.com>
2023-07-04 10:46:57 -0300

    authentication modal and functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,11 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 interface HeaderProps{
   children: React.ReactNode;
@@ -15,11 +20,20 @@ const Header: React.FC<HeaderProps> = ({
   children,
   className,
 }) => {
-
+  const authModal = useAuthModal();
   const router = useRouter()
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser()
 
-  const handleLogout = () => {
-    // handle logout in the future
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    router.refresh();
+
+    if(error){
+      toast.error(error.message)
+    }else{
+      toast.success('Logged out')
+    }
   }
 
   return(
@@ -113,10 +127,26 @@ const Header: React.FC<HeaderProps> = ({
             gap-x-4
           "
         >
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button
+                onClick={handleLogout}
+                className="bg-white px-6 py-2"
+              >
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push("/account")}
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
           <>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="
                   bg-transparent
                   text-neutral-300
@@ -128,7 +158,7 @@ const Header: React.FC<HeaderProps> = ({
             </div>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="
                   bg-white
                   px-6
@@ -139,6 +169,7 @@ const Header: React.FC<HeaderProps> = ({
               </Button>
             </div>
           </>
+          )}
         </div>
       </div>
       {children}
